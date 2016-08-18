4d9b9616229cc269c7b5b3c2697f3bd2724de092
Aditya Kop <ebluegreen@yahoo.com>
2016-08-17 19:31:16 -0700

    Added HashLocationStrategy to routing to fix refresh issue

---
+++ app/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-home',
+  templateUrl: 'templates/home.component.html',
+  styleUrls: ['styles/home.component.css']
+})
+
+export class HomeComponent {
+  
+}
--- app/main.js
+++ app/main.js
@@ -2,21 +2,16 @@
 // Imports for loading & configuring the in-memory web api
 var http_1 = require('@angular/http');
 var angular2_in_memory_web_api_1 = require('angular2-in-memory-web-api');
-//import { InMemoryDataService }               from './in-memory-data.service';
 // The usual bootstrapping imports
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var http_2 = require('@angular/http');
+var common_1 = require('@angular/common');
 var app_component_1 = require('./app.component');
 var app_routes_1 = require('./app.routes');
-/*
-bootstrap(AppComponent, [
-  appRouterProviders,
-  HTTP_PROVIDERS
-]);
-*/
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
     app_routes_1.appRouterProviders,
     http_2.HTTP_PROVIDERS,
     { provide: http_1.XHRBackend, useClass: angular2_in_memory_web_api_1.InMemoryBackendService },
+    { provide: common_1.LocationStrategy, useClass: common_1.HashLocationStrategy }
 ]);
 //# sourceMappingURL=main.js.map
