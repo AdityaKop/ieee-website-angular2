127b1cd2617ecb055eef040c44cd526b9c58013a
Alan Wong <alanwong2357@gmail.com>
2019-01-29 22:58:47 -0800

    added OC IEEE BBQ and Boba Night + Hack Night and Latex Workshop events

--- app/events.component.ts
+++ app/events.component.ts
@@ -3,16 +3,21 @@ declare var M:any;
 
 @Component({
   selector: 'my-events',
-  templateUrl: 'templates/events.component.html',
-  styleUrls: ['styles/events.component.css']
+  templateUrl: '../templates/events.component.html',
+  styleUrls: ['../styles/events.component.css']
 })
 
 export class EventsComponent implements OnInit {
-    options = {};
-    elems = {};
+  options = {};
+  elems = {};
 
-    ngOnInit() {
-        var elems = document.querySelectorAll('.slider');
-	var instances = M.Slider.init(elems, this.options);
-    }
+  ngOnInit() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, this.options);
+  }
+
+  scrollToElement($element): void {
+    console.log($element);
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
 }
